9d2db84b62b6cc25261d3c7cf36a875383228502
DinhTienBo <dinhtienbo2000@outlook.com.vn>
2022-04-30 00:38:18 +0700

    Bai12

---
+++ OganiShop.Data/Repositories/ErrorRepository.cs
@@ -0,0 +1,16 @@
+﻿using OganiShop.Data.Infrastructure;
+using OganiShop.Model.Models;
+
+namespace OganiShop.Data.Repositories
+{
+    public interface IErrorRepository : IRepository<Error>
+    {
+    }
+
+    public class ErrorRepository : RepositoryBase<Error>, IErrorRepository
+    {
+        public ErrorRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
