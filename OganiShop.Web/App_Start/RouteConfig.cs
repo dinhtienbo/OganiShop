3f354215502b56045d1b680a1c3e171ef1f4b829
DinhTienBo <dinhtienbo2000@outlook.com.vn>
2022-05-03 03:44:46 +0700

    Bai43

--- OganiShop.Web/App_Start/RouteConfig.cs
+++ OganiShop.Web/App_Start/RouteConfig.cs
@@ -9,15 +9,42 @@ namespace OganiShop.Web
 {
     public class RouteConfig
     {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+       
+            public static void RegisterRoutes(RouteCollection routes)
+            {
+                routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+                routes.MapRoute(
+             name: "Login",
+             url: "dang-nhap.html",
+             defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional },
+             namespaces: new string[] { "OganiShop.Web.Controllers" }
+         );
+                routes.MapRoute(
+               name: "About",
+               url: "gioi-thieu.html",
+               defaults: new { controller = "About", action = "Index", id = UrlParameter.Optional },
+               namespaces: new string[] { "OganiShop.Web.Controllers" }
+           );
+                routes.MapRoute(
+                 name: "Product Category",
+                 url: "{alias}.pc-{id}.html",
+                 defaults: new { controller = "Product", action = "Category", id = UrlParameter.Optional },
+                   namespaces: new string[] { "OganiShop.Web.Controllers" }
+             );
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                routes.MapRoute(
+                 name: "Product",
+                 url: "{alias}.p-{productId}.html",
+                 defaults: new { controller = "Product", action = "Detail", productId = UrlParameter.Optional },
+                   namespaces: new string[] { "OganiShop.Web.Controllers" }
+             );
+
+                routes.MapRoute(
+                    name: "Default",
+                    url: "{controller}/{action}/{id}",
+                    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                      namespaces: new string[] { "OganiShop.Web.Controllers" }
+                );
+            }
         }
     }
-}
